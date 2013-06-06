f9b3c56fbc85de00ccc94207f5214d2e2e0bb6e0
ncarrizales <nery311084@hotmail.com>
2013-06-05 21:01:17 -0400

    cambiado por nery carrizales

---
+++ JavaApplication24/src/javaapplication24/JavaApplication24.java
@@ -0,0 +1,46 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication24;
+
+/**
+ *
+ * @author Lab-214-21
+ */
+public class JavaApplication24 {
+    public static boolean parImpar (int a, int b, int c)
+    {
+       return !todospares (a,b,c) && !todosimpares (a,b,c);
+        
+    }
+    
+    public static boolean par (int n)
+    {
+        return n % 2 == 0;
+    }
+    public static boolean impar (int n)
+    {
+      return n % 2 != 0;  
+    }
+    
+    public static boolean todospares (int a, int b, int c)
+    {
+     return par(a) && par (b) && par(c);
+     
+    }
+    
+    public static boolean todosimpares (int a, int b, int c)
+    {
+       return impar (a) && impar (b) && impar (c);
+    }
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        System.out.println("a,b,c ¿es un par impar? = "+parImpar (23,24,5));
+        //ffffffffffffffffffffffffffffffffffffffffffffff
+    }
+}
